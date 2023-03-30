046afcf5edcfd3655e7df7d881b3ba0cebb2f07c
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2023-03-31 04:00:40 +0530

    4am/31 march

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app= express();
 const flash = require('connect-flash');
 const axios = require('axios');
 const multer = require('multer');
+const request = require('request');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+// Correct usage with `new` keyword
 
 const fs = require('fs');
 
@@ -37,6 +41,10 @@ app.get("/admin.ejs", function(req, res){
     res.render("admin.ejs");
 });
 
+app.get("/listofteam.ejs", function(req, res){
+    res.render("listofteam.ejs");
+});
+
 
 const memberSchema= new mongoose.Schema({
     name: String,
@@ -49,10 +57,106 @@ const memberSchema= new mongoose.Schema({
   
   // create a model for the data
   const Member = mongoose.model("Member", memberSchema,);
+
+
+
+
+
+
+
   
-  // parse incoming form data
-  app.use(bodyParser.urlencoded({ extended: false }));
+//   // parse incoming form data
+//   app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+//   const Member1 = mongoose.model('Member');
+//   const membersCollection = Member1.collection;
+
+// //   const membersCollection = mongoose.connection.db.collection('members');
+
+//   app.get('/getlist', (req, res) => {
+//     membersCollection.find().toArray(function(err, members) {
+//       if (err) throw err;
   
+//       const addresses = [];
+  
+//       members.forEach(member => {
+//         const api_url = `https://api.tomtom.com/search/2/reverseGeocode/${member.LocationL},${member.LocationA}.json?key=FINkRW9vyyAfiAXdcsM62UKl64oS17Qj&radius=100`;
+  
+//         fetch(api_url)
+//           .then(response => response.json())
+//           .then(data => {
+//             const address = data.address.freeformAddress;
+//             console.log(address);
+//             member.address = address;
+  
+//             // Save the updated member object to the database
+//             membersCollection.save(member);
+  
+//             // Add the address to the list
+//             addresses.push(address);
+  
+//             // If all addresses have been retrieved, send the response
+//             if (addresses.length === members.length) {
+//               res.send(addresses);
+//             }
+//           })
+//           .catch(error => console.log(error));
+//       });
+//     });
+//   });
+  
+
+
+
+
+const fetch = require('node-fetch');
+
+const membersCollection = mongoose.connection.collection('members');
+app.get('/getlist', async (req, res) => {
+    try {
+      // Retrieve all documents from the members collection
+      const members = await membersCollection.find().toArray();
+  
+      // Loop over each document and make a request to the TomTom API
+      const memberAddresses = [];
+      for (const member of members) {
+        const lat = member.LocationL;
+        const lon = member.LocationA;
+        const apiKey = 'FINkRW9vyyAfiAXdcsM62UKl64oS17Qj';
+        const radius = 100;
+      
+        const apiUrl = `https://api.tomtom.com/search/2/reverseGeocode/${lat},${lon}.json?key=${apiKey}&radius=${radius}`;
+  
+        // Make the API request and add the address to the memberAddresses array
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (!data.addresses || data.addresses.length === 0) {
+          console.log('No address found for', lat, lon);
+        } else {
+          const address = data.addresses[0].address.freeformAddress;
+          memberAddresses.push({ name: member.name, email: member.email,contact: member.contact, address: address });
+        }
+      }
+  
+      // Render the HTML template with the memberAddresses array passed as data
+      res.render('getlist', { memberAddresses: memberAddresses });
+    } catch (error) {
+      console.log(error);
+      res.send('An error occurred');
+    }
+  });
+  
+
+
+
+
+
+
+
+
+
   // handle form submission
   app.post('/submit1', (req, res) => {
     if (!req.body.name || !req.body.email) {
