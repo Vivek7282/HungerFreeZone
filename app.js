28f271d6ad0f80448aac7e1d53ca51e4c142cbba
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2023-03-30 22:05:23 +0530

    10pm/30 march

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const config = require('./config');
 const app= express();
 const flash = require('connect-flash');
 const axios = require('axios');
+const multer = require('multer');
 
 const fs = require('fs');
 
@@ -25,13 +26,16 @@ app.get("/", function(req, res){
 });
 app.get("/Contact_Form_with_Fancy_Header_and_Footer.ejs", function(req, res){
     res.render("Contact_Form_with_Fancy_Header_and_Footer.ejs");
-})
+});
 app.get("/Contact_Form_with_Fancy_Header_and_Footer1.ejs", function(req, res){
     res.render("Contact_Form_with_Fancy_Header_and_Footer1.ejs");
-})
+});
 app.get("/Contact_Form_with_Fancy_Header_and_Footer2.ejs", function(req, res){
     res.render("Contact_Form_with_Fancy_Header_and_Footer2.ejs");
-})
+});
+app.get("/admin.ejs", function(req, res){
+    res.render("admin.ejs");
+});
 
 
 const memberSchema= new mongoose.Schema({
@@ -81,7 +85,7 @@ const memberSchema= new mongoose.Schema({
     name: String,
     address: String,
     contact: String,
-    photo: Buffer, // Change the data type to Buffer
+    // photo: Buffer, // Change the data type to Buffer
     LocationL: String,
     LocationA: String
   });
@@ -91,16 +95,18 @@ const memberSchema= new mongoose.Schema({
   
   // parse incoming form data
   app.use(bodyParser.urlencoded({ extended: false }));
+
+  
   
   // handle form submission
   app.post('/submit2', (req, res) => {
     // read the photo file as binary data
-    const photoData = fs.readFileSync(req.body.photo);
+    // const photoData = fs.readFileSync(req.body.photo);
   
     const help = new Help({
       name: req.body.name,
       address: req.body.address,
-      photo: photoData, // assign the binary data to the photo field
+    //   photo: photoData, // assign the binary data to the photo field
       contact: req.body.contact,
       LocationL: req.body.Lo,
       LocationA: req.body.La
@@ -118,6 +124,52 @@ const memberSchema= new mongoose.Schema({
   
 
 
+
+
+
+  const donateSchema = new mongoose.Schema({
+    name: String,
+    address: String,
+    contact: String,
+    item:String,
+    // photo: Buffer, // Change the data type to Buffer
+    LocationL: String,
+    LocationA: String
+  });
+  
+  // create a model for the data
+  const Donate = mongoose.model("Donate", donateSchema,);
+  
+  // parse incoming form data
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  
+  
+  // handle form submission
+  app.post('/submit3', (req, res) => {
+    // read the photo file as binary data
+    // const photoData = fs.readFileSync(req.body.photo);
+  
+    const donate = new Donate({
+      name: req.body.name,
+      address: req.body.address,
+      item: req.body.item,
+    //   photo: photoData, // assign the binary data to the photo field
+      contact: req.body.contact,
+      LocationL: req.body.Lo,
+      LocationA: req.body.La
+    });
+  
+    // save the message to the database
+    donate.save()
+      .then(item => {
+        res.redirect('/');
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  });
+  
 
 
 
